30a233df7499778ecc7c3186c70d20b119c49af3
dantheminotaur <daniel.devine1@outlook.com>
2020-04-08 01:44:36 +0100

    Add Docker Config

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./app ./app
+COPY ./run_telegram_bot.py ./
+
+CMD [ "python", "./run_telegram_bot.py" ]
--- run_telegram_bot.py
+++ run_telegram_bot.py
@@ -1,17 +1,7 @@
-# from app.telegram_connection import TelegramBot
-#
 from app.controller import CoinbaseBotController
 from dotenv import load_dotenv
-from time import sleep
 load_dotenv('.env')
 
 c = CoinbaseBotController()
 
-while True:
-    try:
-        c.check_price()
-        print(c.__dict__)
-        sleep(5)
-    except KeyboardInterrupt:
-        print("closing")
-
+c.start()
