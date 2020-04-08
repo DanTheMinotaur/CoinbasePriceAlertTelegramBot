d7fc132422d68d945ac1796ad4ee742b4fa216b4
dantheminotaur <daniel.devine1@outlook.com>
2020-04-08 02:25:21 +0100

    Fixes to price check logic, update requirements.txt

--- app/controller.py
+++ app/controller.py
@@ -4,6 +4,8 @@ from os import environ
 import json
 from json.decoder import JSONDecodeError
 from datetime import datetime
+from time import sleep
+
 
 def check_config():
     for var in ['BOT_API_KEY', 'CHAT_ID', 'CURRENCY_CODE', 'CRYPTO_CODE', 'CHECK_EVERY', 'PRICE_CHANGE_INCREMENT']:
@@ -20,11 +22,22 @@ class CoinbaseBotController:
         check_config()
         self.td_bot = TelegramBot(api_token=environ['BOT_API_KEY'], chat_id=environ['CHAT_ID'])
         self.check_every = int(environ['CHECK_EVERY'])
-        self.price_change_increment = round(float(environ['PRICE_CHANGE_INCREMENT']), 2)
+        self.price_change_increment = self.round_two(environ['PRICE_CHANGE_INCREMENT'])
         self.currency_code = None
         self.crypto_code = None
         self.__set_currency_codes()
         self.last_price_data: float = self.load_price_from_file()['price']
+
+    def start(self):
+        while True:
+            self.check_price()
+            sleep(self.check_every)
+
+    @staticmethod
+    def round_two(amount: float or int or str) -> float:
+        return round(float(amount), 2)
+
+
 
     def __set_currency_codes(self):
         valid_currencies = get_valid_currency_codes()
@@ -45,15 +58,24 @@ class CoinbaseBotController:
     def check_price(self, check: bool = True):
         price_data = get_price(self.crypto_code, self.currency_code)
         if check:
-            current_amount = round(float(price_data['amount']), 2)
+            current_amount = self.round_two(price_data['amount'])
             price_change = None
-            if (current_amount + self.price_change_increment) >= self.last_price_data:
+            print(f"Current Price {current_amount} \n Last Stored Price {self.last_price_data}")
+            if int(current_amount) >= int(self.last_price_data + self.price_change_increment):
                 price_change = 'increased'
-            elif (current_amount - self.price_change_increment) <= self.last_price_data:
+            elif int(current_amount) <= int(self.last_price_data - self.price_change_increment):
                 price_change = 'decreased'
 
             if price_change:
-                message = f'{self.crypto_code} increased by {self.price_change_increment} is now *{current_amount}{self.currency_code}*'
+                message = "{} {} by _{}_ is now *{}{}*".format(
+                    self.crypto_code,
+                    price_change,
+                    self.round_two(abs(current_amount - self.last_price_data)),
+                    current_amount,
+                    self.currency_code
+                )
+                # message = f'{self.crypto_code} {price_change} by _{self.price_change_increment}_ is now' \
+                #           f' *{current_amount}{self.currency_code}*\nPrevious Price: {self.last_price_data}'
                 self.td_bot.send_message(message, parse_mode='MarkdownV2')
                 self.last_price_data = self.write_price_to_file(current_amount)['price']
 
